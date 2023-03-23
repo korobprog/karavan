2be7b00aacab2a7dac2e9dd7bc8a2fb05cb9c2bf
korobprog <karobprog@gmail.com>
2023-03-24 07:09:48 +1000

    slider correction 5

--- js/caunter.js
+++ js/caunter.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let timer_ = 1680026400
     new FlipDown(timer_, {
         headings: ["ДНИ", "ЧАСЫ", "МИНУТЫ", "СЕКУНДЫ"],
-        theme: "max"
+        theme: "light"
     }).start()
         .ifEnded(() => {
             document.querySelector('.flipdown').innerHTML = `<h1>ОСТАЛОСЬ ДО ПОКАЗА ФИЛЬМА</h1>`;
