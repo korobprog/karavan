e8cf0b92e6e498663ed1fae37898388a8e0a774a
korobprog <karobprog@gmail.com>
2023-03-24 06:11:03 +1000

    slider correction 4

---
+++ js/caunter.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let timer_ = 1680026400
+    new FlipDown(timer_, {
+        headings: ["ДНИ", "ЧАСЫ", "МИНУТЫ", "СЕКУНДЫ"],
+        theme: "max"
+    }).start()
+        .ifEnded(() => {
+            document.querySelector('.flipdown').innerHTML = `<h1>ОСТАЛОСЬ ДО ПОКАЗА ФИЛЬМА</h1>`;
+        })
+});
